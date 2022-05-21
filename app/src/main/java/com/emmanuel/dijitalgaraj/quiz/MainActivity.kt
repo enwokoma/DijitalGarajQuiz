d5b30042d92c70062be5d7bd2032f89cb88a4071
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2022-05-21 20:32:17 +0100

    Catch Exceptions
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/emmanuel/dijitalgaraj/quiz/MainActivity.kt
+++ app/src/main/java/com/emmanuel/dijitalgaraj/quiz/MainActivity.kt
@@ -1,26 +1,21 @@
 package com.emmanuel.dijitalgaraj.quiz
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.annotation.RequiresApi
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.emmanuel.dijitalgaraj.quiz.databinding.ActivityMainBinding
 import com.emmanuel.dijitalgaraj.quiz.interfaces.InterfaceAPI
-import com.emmanuel.dijitalgaraj.quiz.utils.TinyDB
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
+import retrofit2.HttpException
 import retrofit2.Retrofit
-import java.util.concurrent.CompletableFuture
-
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -33,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         binding.putButton.setOnClickListener { fetchHash() }
     }
 
-    private fun fetchHash() {
+    private fun fetchHash() = GlobalScope.launch(Dispatchers.Main) {
         val url = "http://career.dijitalgaraj.com/hash/"
         val slug = "emmanuel-nwokoma-40866"
         val guid = "80e2c70f-996b-445d-95f5-2f1822145397"
@@ -56,38 +51,45 @@ class MainActivity : AppCompatActivity() {
         // Create RequestBody (I am not using any converter, like GsonConverter, MoshiConverter e.t.c, that's why we use RequestBody )
         val requestBody = jsonObjectString.toRequestBody("application/json".toMediaTypeOrNull())
 
-        CoroutineScope(Dispatchers.IO).launch {
-
-            // Do the PUT request and get response
-            val response = service.updateRecord(requestBody)
-
-            withContext(Dispatchers.Main) {
-                if (response.isSuccessful) {
-
-                    // Convert raw JSON to pretty JSON using GSON library
-                    val gson = GsonBuilder().setPrettyPrinting().create()
-                    val prettyJson = gson.toJson(
-                        JsonParser.parseString(
-                            response.body()
-                                ?.string() // About this thread blocking annotation : https://github.com/square/retrofit/issues/3255
-                        )
-                    )
-                    Log.d("Pretty Printed JSON :", prettyJson)
-
-                    val intent = Intent(this@MainActivity, ShowInformationActivity::class.java)
-                    intent.putExtra("json_results", prettyJson)
-
-                    // Get Hash from JSON
-                    val getHash = JSONObject(prettyJson).get("hash").toString()
-                    intent.putExtra("hashKey", getHash)
-                    this@MainActivity.startActivity(intent)
-
-                } else {
-
-                    Log.e("RETROFIT_ERROR", response.code().toString())
-
-                }
+        try {
+            val response = withContext(Dispatchers.IO) {
+                // Do the PUT request and get response
+                service.updateRecord(requestBody)
             }
+
+            //Do something with response e.g show to the UI.
+            if (response.isSuccessful) {
+
+                // Convert raw JSON to pretty JSON using GSON library
+                val gson = GsonBuilder().setPrettyPrinting().create()
+                val prettyJson = gson.toJson(
+                    JsonParser.parseString(
+                        response.body()
+                            ?.string() // About this thread blocking annotation : https://github.com/square/retrofit/issues/3255
+                    )
+                )
+                Log.d("Pretty Printed JSON :", prettyJson)
+
+                val intent = Intent(this@MainActivity, ShowInformationActivity::class.java)
+                intent.putExtra("json_results", prettyJson)
+
+                // Get Hash from JSON
+                val getHash = JSONObject(prettyJson).get("hash").toString()
+                intent.putExtra("hashKey", getHash)
+                this@MainActivity.startActivity(intent)
+
+            } else {
+
+                Log.e("RETROFIT_ERROR", response.code().toString())
+
+            }
+        }
+        catch (e: HttpException) {
+            Toast.makeText(this@MainActivity, "Exception ${e.message}", Toast.LENGTH_LONG).show()
+        } catch (e: IOException) {
+            Toast.makeText(this@MainActivity, "No Internet Connection, try again", Toast.LENGTH_LONG).show()
+        } catch (e: Throwable) {
+            Toast.makeText(this@MainActivity, "Ooops: Something else went wrong", Toast.LENGTH_LONG).show()
         }
     }
 }
